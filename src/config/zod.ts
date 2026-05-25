fabb7feab89b3bf8788e2174a101f711fec513ed
pongphol424 <pongphol.kimm@gmail.com>
2026-05-25 13:15:28 +0700

    validation: add a new custom error code invalid_format to return an error massage

--- src/config/zod.ts
+++ src/config/zod.ts
@@ -10,6 +10,10 @@ z.config({
             return `invalid type, ${String(iss.path[0])} expected ${iss.expected}`;
         }
 
+        if(iss.code === "invalid_format" && iss.path){
+            return `${String(iss.path[0])} is invalid format`
+        }
+
         if(iss.code === "too_small" && iss.path){
             if(iss.origin === "number"){
                 return `${String(iss.path[0])} must be at least ${iss.minimum}`
