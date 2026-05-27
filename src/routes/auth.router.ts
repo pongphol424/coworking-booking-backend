1a5c487227ed4ca9eb99c7a3a1f2c2d55b5ba679
pongphol424 <pongphol.kimm@gmail.com>
2026-05-27 20:14:55 +0700

    refactor: rename router referenc

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { validate } from '../middlewares/validate.middleware';
-import { auth, login, logout, register } from '../controller/auth';
+import { authUser, login, logout, register } from '../controller/auth';
 import { loginSchema, registerSchema } from '../schema/auth.schema';
 import { isAuthenticated } from '../middlewares/auth.middleware';
 
@@ -10,7 +10,7 @@ const router = Router();
 router.post('/register',validate(registerSchema),register);
 router.post('/login',validate(loginSchema),login);
 router.post('/logout',logout);
-router.get('/authUser',isAuthenticated,auth)
+router.get('/authUser',isAuthenticated,authUser)
 
 
 export default router;
