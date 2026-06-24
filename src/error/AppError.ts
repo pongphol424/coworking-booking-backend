4937bacf56e8d66020bccfdbc0c2a18c1342300b
pongphol424 <pongphol.kimm@gmail.com>
2026-06-24 15:11:06 +0700

    refactor: remove redundant message assignment

--- src/error/AppError.ts
+++ src/error/AppError.ts
@@ -3,7 +3,6 @@ export class AppError extends Error{
     public error?: unknown;
     constructor(message: string, status: number , error?: unknown){
         super(message)
-        this.message = message
         this.status = status
         this.error = error
     }
