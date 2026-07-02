07ef201319c0b84dea153d75700da4721c1d4e95
pongphol424 <pongphol.kimm@gmail.com>
2026-07-02 16:09:51 +0700

    refactor: handle duplicate phone number and correact response property name

--- src/error/handlers/dbError.handler.ts
+++ src/error/handlers/dbError.handler.ts
@@ -3,24 +3,32 @@
 
 export const handleDbError = (err: any) => {
     if (err?.cause?.code === "ER_DUP_ENTRY") {
-        if (err?.cause?.message.includes("room_types_name_unique")) {
+        if (err.cause.message.includes("room_types_name_unique")) {
             return {
                 status: 409,
                 code: "ROOM_TYPE_NAME_ALREADY_EXISTS",
                 message: "RoomType name is already exists"
             };
         }
-        if (err.cause?.message.includes("rooms_room_number_unique"))
+        if (err.cause.message.includes("rooms_room_number_unique")){
             return {
                 status: 409,
                 code: "ROOM_NUMBER_ALREADY_EXISTS",
                 message: "Room Number already exists"
             }
-        if (err?.cause?.message.includes("users.users_email_unique")) {
+        }
+        if (err.cause.message.includes("users.users_email_unique")) {
             return {
                 status: 409,
                 code: "EMAIL_ALREADY_EXISTS",
                 message: "Email already exists",
+            };
+        }
+        if (err.cause.message.includes("users.users_phone_number_unique")) {
+            return {
+                status: 409,
+                code: "PHONE_NUMBER_ALREADY_EXISTS",
+                message: "Phone number already exists",
             };
         }
         return {
@@ -37,7 +45,7 @@ export const handleDbError = (err: any) => {
             status: 400,
             code: "INVALID_FIELD_VALUE",
             message: "Invalid value for field",
-            ...(err.cause.message && { error: err.cause.message }),
+            ...(err.cause.message && { errorCause: err.cause.message }),
             ...(err.cause.sql && { sql: err.cause.sql })
         }
     }
