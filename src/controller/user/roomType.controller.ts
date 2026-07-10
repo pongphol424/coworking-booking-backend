0ee95c17260fcdb3227599b1f9ea115cb0b638a3
pongphol424 <pongphol.kimm@gmail.com>
2026-07-10 20:05:06 +0700

    fix: filter room types by current status

--- src/controller/user/roomType.controller.ts
+++ src/controller/user/roomType.controller.ts
@@ -3,57 +3,72 @@ import db from '../../config/db'
 import roomTypes from '../../db/schema/room_types';
 import facilities from '../../db/schema/facilities';
 import roomTypesFacilities from '../../db/schema/room_types_facilities';
-import {ne, eq, inArray } from 'drizzle-orm';
+import { ne, eq, sql, and, lte, or, gt, isNull } from 'drizzle-orm';
 import roomTypeStatusHistory from '../../db/schema/room_type_status_history';
 import roomStatusTypes from '../../db/schema/room_status_types';
+import { AppError } from '../../error/AppError';
 
 
 
 export const getRoomType = async (req: Request, res: Response) => {
-    try {
-        const availableRoomTypeIdsQuery = await db
-            .selectDistinct({ roomTypeId: roomTypeStatusHistory.roomTypeId })
-            .from(roomTypeStatusHistory)
-            .where(ne(roomTypeStatusHistory.statusTypeId,4))
-
-        const availableRoomTypeIds: number[] = availableRoomTypeIdsQuery.map(
-            (n) => n.roomTypeId
-        )
-
-        const result = await db.select({ roomTypes, facilityName: facilities.name })
-            .from(roomTypes)
-            .where(inArray(roomTypes.id, availableRoomTypeIds))
-            .leftJoin(roomTypesFacilities, eq(roomTypes.id, roomTypesFacilities.roomTypeId))
-            .leftJoin(facilities, eq(roomTypesFacilities.facilityId, facilities.id))
-        if (!result.length) {
-            return res.status(404).json({
-                message: "RoomType not found"
-            })
-        }
-
-        const roomTypeMap: { [key: number]: any } = {}
-        for (let i = 0; i < result.length; i++) {
-            const id = result[i].roomTypes.id
-            if (!roomTypeMap[id]) {
-                roomTypeMap[id] = {
-                    ...result[i].roomTypes,
-                    facilities: []
-                }
-            }
-            if (result[i].facilityName) {
-                roomTypeMap[id].facilities.push(result[i].facilityName)
-            }
-        }
-
-        const roomTypeList = Object.values(roomTypeMap)
-
-        res.json({
-            message: res.locals.message,
-            roomTypeList
+    const date = new Date();
+    const subCurrentRoomTypeStatusMaxPriority = db
+        .select({
+            roomTypeId: roomTypeStatusHistory.roomTypeId,
+            maxPriority: sql<number>`MAX(${roomStatusTypes.priority})`.as("maxPriority")
         })
-    } catch (error) {
-        res.status(400).json(error)
+        .from(roomTypeStatusHistory)
+        .where(
+            and(
+                ne(roomTypeStatusHistory.statusTypeId, 4),
+                lte(roomTypeStatusHistory.startDate, date),
+                or(
+                    gt(roomTypeStatusHistory.endDate, date),
+                    isNull(roomTypeStatusHistory.endDate)
+                )
+            )
+        )
+        .innerJoin(roomStatusTypes,
+            eq(roomTypeStatusHistory.statusTypeId, roomStatusTypes.id)
+        )
+        .groupBy(roomTypeStatusHistory.roomTypeId)
+        .as("currentRoomTypeStatus");
+
+    const roomTypeResults = await db
+        .select({
+            roomType: roomTypes,
+            facilityName: facilities.name,
+            statusName: roomStatusTypes.name
+        })
+        .from(roomTypes)
+        .innerJoin(subCurrentRoomTypeStatusMaxPriority,
+            eq(subCurrentRoomTypeStatusMaxPriority.roomTypeId, roomTypes.id)
+        )
+        .innerJoin(roomStatusTypes,
+            eq(roomStatusTypes.priority, subCurrentRoomTypeStatusMaxPriority.maxPriority)
+        )
+        .leftJoin(roomTypesFacilities, eq(roomTypes.id, roomTypesFacilities.roomTypeId))
+        .leftJoin(facilities, eq(roomTypesFacilities.facilityId, facilities.id));
+
+    const roomTypeMap: Record<number,any> = {}
+    for (let i = 0; i < roomTypeResults.length; i++) {
+        const id = roomTypeResults[i].roomType.id
+        if (!roomTypeMap[id]) {
+            roomTypeMap[id] = {
+                ...roomTypeResults[i].roomType,
+                statusName: roomTypeResults[i].statusName,
+                facilities: []
+            }
+        }
+        if (roomTypeResults[i].facilityName) {
+            roomTypeMap[id].facilities.push(roomTypeResults[i].facilityName)
+        }
     }
+
+    const roomTypeList = Object.values(roomTypeMap)
+    res.json({
+        roomTypeList
+    })
 }
 
 
