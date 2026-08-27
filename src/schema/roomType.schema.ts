1b3903b1b44a5ca8dc1d9d27147d39db1386e8f3
pongphol424 <pongphol.kimm@gmail.com>
2026-08-27 18:19:45 +0700

    feat: add new schema roomTypeWithStatus

--- src/schema/roomType.schema.ts
+++ src/schema/roomType.schema.ts
@@ -1,4 +1,5 @@
 import * as z from 'zod';
+import facilities from '../db/schema/facilities';
 // import {} from './status.schema';
 
 
@@ -15,8 +16,14 @@ export const roomTypeCreate = roomTypeBaseSchema.extend({
 
 export const roomTypeUpdate = roomTypeCreate.extend({}).partial()
 
+export const roomTypeWithStatus = roomTypeBaseSchema.extend({
+    statusName: z.string(),
+    description: z.string().nullable(),
+    facilities: z.array(z.string().nullable())
+})
+
 
 export type RoomTypeBaseDto = z.infer<typeof roomTypeBaseSchema>;
 export type RoomTypeCreateDto = z.infer<typeof roomTypeCreate>;
 export type RoomTypeUpdateDto = z.infer<typeof roomTypeUpdate>;
-
+export type RoomTypeWithStatusDto = z.infer<typeof roomTypeWithStatus>;
